34d760996612a52015e910ef3203dcb33bd83842
Jose <jmlb0003@gmail.com>
2018-02-22 00:27:11 +0100

    Added implementation of the method to populate the view with the sandwich details and hide the empty fields

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,7 +3,9 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -27,7 +29,10 @@ public class DetailActivity extends AppCompatActivity {
             closeOnError();
         }
 
-        int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
+        int position = DEFAULT_POSITION;
+        if (intent != null) {
+            position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
+        }
         if (position == DEFAULT_POSITION) {
             // EXTRA_POSITION not found in intent
             closeOnError();
@@ -43,7 +48,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +61,37 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(final Sandwich sandwich) {
+        if (sandwich.getAlsoKnownAs() != null && sandwich.getAlsoKnownAs().size() > 0) {
+            final TextView alsoKnownAsTitle = findViewById(R.id.also_known_as_title);
+            alsoKnownAsTitle.setVisibility(View.VISIBLE);
+            final TextView alsoKnownAs = findViewById(R.id.also_known_as);
+            alsoKnownAs.setVisibility(View.VISIBLE);
+            alsoKnownAs.setText(sandwich.getAlsoKnownAs().toString());
+        }
 
+        if (sandwich.getPlaceOfOrigin() != null && !sandwich.getPlaceOfOrigin().isEmpty()) {
+            final TextView originTitle = findViewById(R.id.origin_title);
+            originTitle.setVisibility(View.VISIBLE);
+            final TextView origin = findViewById(R.id.origin);
+            origin.setVisibility(View.VISIBLE);
+            origin.setText(sandwich.getPlaceOfOrigin());
+        }
+
+        if (sandwich.getDescription() != null && !sandwich.getDescription().isEmpty()) {
+            final TextView descriptionTitle = findViewById(R.id.description_title);
+            descriptionTitle.setVisibility(View.VISIBLE);
+            final TextView description = findViewById(R.id.description);
+            description.setVisibility(View.VISIBLE);
+            description.setText(sandwich.getDescription());
+        }
+
+        if (sandwich.getIngredients() != null && sandwich.getIngredients().size() > 0) {
+            final TextView ingredientsTitle = findViewById(R.id.ingredients_title);
+            ingredientsTitle.setVisibility(View.VISIBLE);
+            final TextView ingredients = findViewById(R.id.ingredients);
+            ingredients.setVisibility(View.VISIBLE);
+            ingredients.setText(sandwich.getIngredients().toString());
+        }
     }
 }
